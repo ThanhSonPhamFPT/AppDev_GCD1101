7aa742a06e5e95b59dcabf3f8282bb64db6e2452
Pham Thanh Son <sonpt28@fpt.edu.vn>
2023-12-05 13:54:23 +0700

    Add Country To Application User

--- BookShop.Models/ApplicationUser.cs
+++ BookShop.Models/ApplicationUser.cs
@@ -9,5 +9,6 @@ namespace BookShopWeb.Models
         public string Name { get; set; }
         public string? Address {  get; set; }
         public string? City { get; set; }
+        public string? Country { get; set; }
     }
 }
